992fda0b82cd461cc9181fe65134eab8f7a1418c
Kai Brügge <kai.brugge@cs.helsinki.fi>
2014-11-17 17:34:39 +0200

    code and notebook for nonstationary ODE kernel

--- kern/__init__.py
+++ kern/__init__.py
@@ -5,5 +5,6 @@ from _src.heat_eqinit import Heat_eqinit
 
 from _src.ode1_lfm import Ode1_lfm
 from _src.ode1_eq_lfm import Ode1_eq_lfm
+from _src.ode1_edeq_lfm import Ode1_edeq_lfm
 #from _src.ode1 import Ode1
 
