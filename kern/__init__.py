8e30a251d514c27dbfb6ca84158336ca5deb1d3c
Kai Brügge <kai.brugge@cs.helsinki.fi>
2014-11-28 16:23:23 +0200

    added switching point latent kernel

--- kern/__init__.py
+++ kern/__init__.py
@@ -8,3 +8,7 @@ from _src.ode1_eq_lfm import Ode1_eq_lfm
 from _src.ode1_edeq_lfm import Ode1_edeq_lfm
 #from _src.ode1 import Ode1
 
+
+from _src.sage_ode1_eq_lfm import Sage_Ode1_eq_lfm
+
+from _src.ode1_sp_lfm import Ode1_sp_ldm
---
+++ kern/_src/ode1_sp_ldm.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sympy as sym
+import GPy 
+from sympy import sqrt, exp, erf
+
+from ode1_lfm import Ode1_lfm
+from ...util.symbolic import *
+
+
+
+
+
+class Ode1_edsp_lfm(Ode1_lfm):
+    """
+    A symbolic covariance based on a first order differential equation being driven by a latent force that smoothly switches between two random levels. 
+
+    """
+    def __init__(self, output_dim=1, variance=1, lengthscale=1, scale=1, decay=1, switchingpoint=0, name='Ode1_edsp_lfm', cse=True):
+
+        parameters = {'variance' : variance, 'lengthscale' : lengthscale, 'scale'=scale, 'decay'=decay, 'switchingpoint': switchingpoint}
+
+        input_dim = 2
+
+        x_0, z_0, variance, lengthscale, sp = sym.symbols('x_0, z_0, variance, lengthscale, sp', positive=True)
+        scale = sym.symbols('scale')
+
+
+        transition = Piecewise( (1, Le(x,sp)), (exp(-x) / lengthscale, True) )
+        
+        k_uu = variance * (transition.subs(x,x_0) * transition.subs(x,z_0) + (1-transition.subs(x,x_0)) * (1-transition.subs(x,z_0)))
+
+
+
+        super(Ode1_eq_lfm, self).__init__(k_uu, output_dim=output_dim, parameters=parameters, name=name, func_modules=[{'differfln':GPy.util.functions.differfln}], cse=cse)
